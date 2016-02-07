485312b1946c84ff2b955524e7eeab1e384ced20
Peter Tran <pete.tran02@gmail.com>
2016-02-07 09:29:49 -0500

    Screen saver added.

--- gettingstarted/static/main.js
+++ gettingstarted/static/main.js
@@ -84,12 +84,11 @@ app.controller('MainController', function($scope, $http) {
   };
 
   main.updateProgressBar = function ($index) {
-    console.log("goal: " + main.goals[$index].goal)
     var getPercent = ( main.goals[$index].balance / main.goals[$index].goal);
     var getProgressWrapWidth = $('.progress-wrap-' + $index).width();
     var progressTotal = getPercent * getProgressWrapWidth;
     var animationLength = getPercent * 1500;
-    
+
     // on page load, animate percentage bar to data percentage length
     // .stop() used to prevent animation queueing
     $('.progress-bar-' + $index).stop().animate({
@@ -98,4 +97,56 @@ app.controller('MainController', function($scope, $http) {
     }, animationLength);
   };
 });
+
+/* Screen saver */
+var mousetimeout;
+var screensaver_active = true;
+var idletime = 20;
+
+function show_screensaver(){
+    $('#screensaver').fadeIn();
+    screensaver_active = true;
+    screensaver_animation();
+}
+
+function stop_screensaver(){
+    $('#screensaver').fadeOut();
+    screensaver_active = false;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+$(document).mousemove(function(){
+
+    clearTimeout(mousetimeout);
+  
+    if (screensaver_active) {
+        stop_screensaver();
+    }
+
+    mousetimeout = setTimeout(function(){
+        show_screensaver();
+    }, 1000 * idletime); // 5 secs      
+});
+
+function screensaver_animation(){
+    if (screensaver_active) {
+        $('#screensaver').animate(
+            {backgroundColor: getRandomColor()},
+            400,
+            screensaver_animation);
+    }
+}
+
+
+
+
+
 
